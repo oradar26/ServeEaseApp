9b53dc13b9490dd9110ef1dab986f2f0b82c9f14
oradar26 <134605973+oradar26@users.noreply.github.com>
2023-08-06 08:54:09 +0300

    .

---
+++ app/src/main/java/com/example/serveeaseapp/WaiterDashboard.java
@@ -0,0 +1,14 @@
+package com.example.serveeaseapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WaiterDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waiter_dashboard);
+    }
+}
